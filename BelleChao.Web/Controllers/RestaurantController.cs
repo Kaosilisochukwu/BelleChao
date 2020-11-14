a3f4ea1c90fff6b52fc2e5309ed37e081c807631
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-14 03:53:32 +0100

    Add controller and view for user registration

---
+++ BelleChao.Web/Controllers/RestaurantController.cs
@@ -0,0 +1,21 @@
+﻿using BelleChao.Data.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelleChao.Web.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Register([FromForm] RestaurantToRegisterDTO model)
+        {
+            return View();
+        }
+    }
+}
