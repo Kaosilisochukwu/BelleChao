9a612301516f103785cb802f09b3086f1b673a81
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-16 21:48:31 +0100

    Add settings for cloudinary

---
+++ BelleChao.Data/Models/CloudinarySettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BelleChao.Data.Models
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
