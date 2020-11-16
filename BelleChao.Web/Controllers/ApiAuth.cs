14b7af7701d6f3f7b170493a06bb25479e3d9b83
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-16 22:24:39 +0100

    Add Api controllers for the home page and authentication

---
+++ BelleChao.Web/Controllers/ApiAuth.cs
@@ -0,0 +1,34 @@
+﻿using BelleChao.Data.Models;
+using BelleChao.Data.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelleChao.Web.Controllers
+{
+    [Route("api/register")]
+    [ApiController]
+    public class ApiAuth : ControllerBase
+    {
+        private readonly IRestaurantRepository _restaurantRepo;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public ApiAuth(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IRestaurantRepository restaurantRepo)
+        {
+            _restaurantRepo = restaurantRepo;
+            _userManager = userManager;
+           _signInManager = signInManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var restaurants = await _restaurantRepo.GetRestaurants();
+            return Ok(restaurants);
+        }
+    }
+}
---
+++ BelleChao.Web/Controllers/ApiHome.cs
@@ -0,0 +1,26 @@
+﻿using BelleChao.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelleChao.Web.Controllers
+{
+    [Route("api/home")]
+    [ApiController]
+    public class ApiHome : ControllerBase
+    {
+        private readonly IRestaurantRepository _restaurantRepo;
+
+        public ApiHome(IRestaurantRepository restaurantRepo)
+        {
+            _restaurantRepo = restaurantRepo;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var restaurants = await _restaurantRepo.GetRestaurants();
+            return Ok(restaurants);
+        }
+    }
+}
