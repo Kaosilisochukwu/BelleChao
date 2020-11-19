198dc3dcc2f2254f04ffc61fde90b37a49915beb
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-19 01:56:28 +0100

    Add JWT token generator method

---
+++ BelleChao.Web/Utilities/TokenConfigurations.cs
@@ -0,0 +1,46 @@
+﻿using BelleChao.Data.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace BelleChao.Web.Utilities
+{
+    public class TokenConfigurations
+    {
+        public string GenerateToken(ApplicationUser user, IConfiguration config, List<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim("userName", user.UserName)
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role));
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config.GetSection("Jwt:SigninKey").Value));
+
+            var credential = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                SigningCredentials = credential,
+                Expires = DateTime.Now.AddDays(1)
+             };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
