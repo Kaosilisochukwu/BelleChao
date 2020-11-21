443ab586cabbce3b1534ac000e9ea5f63bd54b7f
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-21 23:24:57 +0100

    Add API controllers for Order and Restaurant

---
+++ BelleChao.Web/Controllers/ApiOrderController.cs
@@ -0,0 +1,31 @@
+﻿using BelleChao.Data;
+using BelleChao.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelleChao.Web.Controllers
+{
+    [ApiController]
+    [Route("api/order")]
+    public class ApiOrderController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ApiOrderController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> PlaceOder(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.Orders.AddAsync(order);
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ BelleChao.Web/Controllers/ApiRestaurant.cs
@@ -0,0 +1,31 @@
+﻿using BelleChao.Data;
+using BelleChao.Data.DTOs;
+using BelleChao.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelleChao.Web.Controllers
+{
+    public class ApiRestaurant : ControllerBase
+    {
+        private readonly IRestaurantRepository _restrurantRepo;
+
+        public ApiRestaurant(IRestaurantRepository restrurantRepo)
+        {
+            _restrurantRepo = restrurantRepo;
+        }
+
+        [HttpPost]
+        public IActionResult Index(RestaurantToRegisterDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                _restrurantRepo.AddRestaurant(model);
+            }
+            return Ok();
+        }
+    }
+}
