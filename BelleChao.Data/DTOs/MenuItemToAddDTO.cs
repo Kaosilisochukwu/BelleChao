af9a19ac2532be5dfad7af16c41ca43949b84c79
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-21 23:25:31 +0100

    create metu item DTO

---
+++ BelleChao.Data/DTOs/MenuItemToAddDTO.cs
@@ -0,0 +1,31 @@
+﻿using BelleChao.Data.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BelleChao.Data.DTOs
+{
+    public class MenuItemToAddDTO
+    {
+        [Required]
+        [DisplayName("Restaurant Id")]
+        public string RestaurantId { get; set; }
+        public Restaurant Restaurant { get; set; }
+
+        [Required]
+        [DisplayName("Restaurant Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please give a brief description of this item")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please give a brief description of this item")]
+        public IFormFile PhotoUrl { get; set; }
+
+        [Required]
+        public decimal UnitPrice { get; set; }
+    }
+}
