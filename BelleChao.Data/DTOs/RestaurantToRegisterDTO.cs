ba9ef4763785fdea20b76061e7b7ebb6d26fddb1
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-11-21 23:25:53 +0100

    Update Restraurant to register DTO

--- BelleChao.Data/DTOs/RestaurantToRegisterDTO.cs
+++ BelleChao.Data/DTOs/RestaurantToRegisterDTO.cs
@@ -1,6 +1,8 @@
 ﻿using BelleChao.Data.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -23,5 +25,16 @@ namespace BelleChao.Data.DTOs
         [Required(ErrorMessage = "State Field is required", AllowEmptyStrings = false)]
         [MaxLength(100, ErrorMessage = "State Field is must not exceed 100 Characters")]
         public string State { get; set; }
+
+        [Required(ErrorMessage ="Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Phone]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public IFormFile Photo { get; set; }
     }
 }
