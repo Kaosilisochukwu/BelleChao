100d740330b44f7ac81a755a37fcd7bed1a57208
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-02 23:47:11 +0100

    add model for api response message and restaurant to update

---
+++ BelleChao.Data/DTOs/RestaurantToUpdateDTO.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BelleChao.Data.DTOs
+{
+    public class RestaurantToUpdateDTO
+    {
+
+        [Required(ErrorMessage = "Restaurant Name is required", AllowEmptyStrings = false)]
+        [MaxLength(100, ErrorMessage = "Restaurant Name must not exceed 100 Characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Restaurant Address is required", AllowEmptyStrings = false)]
+        [MaxLength(100, ErrorMessage = "Restaurant Address must not exceed 100 Characters")]
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "City Field is required", AllowEmptyStrings = false)]
+        [MaxLength(100, ErrorMessage = "City Field is must not exceed 100 Characters")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "State Field is required", AllowEmptyStrings = false)]
+        [MaxLength(100, ErrorMessage = "State Field is must not exceed 100 Characters")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Phone Number is required")]
+        [Phone]
+        [DisplayName("Phone Number")]
+        public string PhoneNumber { get; set; }
+
+        public string PhotoUrl { get; set; }
+
+        public string PhotoPublicId { get; set; }
+
+        public int Rating { get; set; }
+    }
+}
---
+++ BelleChao.Data/Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BelleChao.Data.Models
+{
+    public class ResponseModel
+    {
+        public int StatusCode { get; set; }
+        public string Description { get; set; }
+        public object Data { get; set; }
+    }
+}
