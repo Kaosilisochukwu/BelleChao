810b7911f7e85ec25c4a19bcacb515353c04d5b2
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-04 22:01:11 +0100

    update migrations

---
+++ BelleChao.Data/Migrations/20201203005538_orderAmount.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BelleChao.Data.Migrations
+{
+    public partial class orderAmount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalAmount",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalAmount",
+                table: "Orders");
+        }
+    }
+}
