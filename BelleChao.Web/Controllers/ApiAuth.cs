2ed080e46faddb63f7aa477e93ccb9c362bbcf76
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-08 23:29:00 +0100

    add code for user registration and login

--- BelleChao.Web/Controllers/ApiAuth.cs
+++ BelleChao.Web/Controllers/ApiAuth.cs
@@ -1,23 +1,34 @@
-﻿using BelleChao.Data.DTOs;
+﻿using AutoMapper;
+using BelleChao.Data.DTOs;
 using BelleChao.Data.Models;
 using BelleChao.Data.Services;
+using BelleChao.Web.Utilities;
+using CloudinaryDotNet.Actions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace BelleChao.Web.Controllers
 {
-    [Route("api")]
+    [Route("api/user")]
     [ApiController]
     public class ApiAuth : ControllerBase
     {
         private readonly IRestaurantRepository _restaurantRepo;
+        private readonly IConfiguration _configuration;
+        private readonly IMapper _mapper;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public ApiAuth(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, IRestaurantRepository restaurantRepo)
+        public ApiAuth(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager,
+                                            IRestaurantRepository restaurantRepo,
+                                            IConfiguration configuration, IMapper mapper)
         {
             _restaurantRepo = restaurantRepo;
+            _configuration = configuration;
+            _mapper = mapper;
             _userManager = userManager;
            _signInManager = signInManager;
         }
@@ -30,18 +41,32 @@ namespace BelleChao.Web.Controllers
 
         [HttpPost]
         [Route("register")]
-        public  IActionResult Register(UserToRegisterDTO model)
+        public  async Task<IActionResult> Register(UserTOPostDTO model)
         {
-            var mode = model;
-            return Ok();
+            if (ModelState.IsValid)
+            {
+                var user = _mapper.Map<UserTOPostDTO, ApplicationUser>(model);
+                var creationResult = await _userManager.CreateAsync(user, model.Password);
+            }
+            return BadRequest();
         }
 
         [HttpPost]
         [Route("login")]
-        public IActionResult Login(UserToLoginDTO model)
+        public async Task<IActionResult> Login(UserToLoginDTO model)
         {
-            var loginModel = ModelState;
-            return Ok();
+            if (ModelState.IsValid)
+            {
+                var applicationUser = await _userManager.FindByEmailAsync(model.Email);
+                var signinResult = await _signInManager.PasswordSignInAsync(applicationUser, model.Password, model.IsPersistent, false);
+                if (signinResult.Succeeded)
+                {
+                    var roles = (await _userManager.GetRolesAsync(applicationUser)).ToList();
+                    var token = TokenConfigurations.GenerateToken(applicationUser, _configuration, roles);
+                    return Ok(token);
+                }
+            }
+            return BadRequest();
         }
     }
 }
